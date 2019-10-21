e3638ffca2623f8c8acc51400cf77c53b417936c
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-21 02:13:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,13 +15,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly_with_third_tier = {
-    :railroads => {
+  monopoly_with_third_tier = :railroads => {
       :pieces => 4, :rent_in_dollars => {
         :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
-      },
-      :names => {
-        :reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railraod => {"mortgage_value" => "$400"}, :shortline_railroad => {"mortgage_value" => "$800"}
+      }, :names => {
+        :reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railraod => {}, :shortline_railroad => {}
       }
     }
   }
@@ -33,11 +31,12 @@ end
 
 def monopoly_with_fourth_tier
   monopoly_with_fourth_tier = {
-    :railroads => {
+      :railroads => {
       :pieces => 4, :rent_in_dollars => {
         :one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200
-      }, :names => {
-        :reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railraod => {}, :shortline_railroad => {}
+      },
+      :names => {
+        :reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railraod => {"mortgage_value" => "$400"}, :shortline_railroad => {"mortgage_value" => "$800"}
       }
     }
   }
