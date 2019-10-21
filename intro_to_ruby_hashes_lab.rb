d4e4f3c048be30d7e88dd1f979578226b3cab49e
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-21 02:02:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 
 def monopoly_with_third_tier
   monopoly_with_third_tier = {
-    :railroads => {:pieces => 4, :rent_in_dollars => {}
+    :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
     }
   }
   # When you start writing the implementation for this method, copy the Hash
